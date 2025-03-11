fa8c8971fc5ab6d631b56934d044077a9084a91e
mathieumarcelino <marcelinomathieu91@gmail.com>
2025-03-11 16:20:21 +0100

    Route correction for different countries

--- src/App.js
+++ src/App.js
@@ -3,18 +3,32 @@ import './App.css';
 import Playlist from './Components/Playlist/Playlist';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
-import {AppProvider} from "./Context/AppContext"
+import { AppProvider } from "./Context/AppContext"
+import { Routes, Route, Navigate } from 'react-router-dom';
 // ------------------
 
 function App() {
+  const validCountries = ['us', 'fr', 'de', 'es', 'pt', 'it'];
   
+  const MainContent = () => (
+    <>
+      <Header/>
+      <Playlist/>
+      <Footer/>
+    </>
+  );
+
   return (
     <div className="main">
-        <AppProvider>
-          <Header/>
-          <Playlist/>
-          <Footer/>
-        </AppProvider>
+      <AppProvider>
+        <Routes>
+          <Route path="/" element={<MainContent />} />
+          {validCountries.map(country => (
+            <Route key={country} path={`/${country}`} element={<MainContent />} />
+          ))}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </AppProvider>
     </div>
   );
 }
